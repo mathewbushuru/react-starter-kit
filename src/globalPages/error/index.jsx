1c5425e6b0e0842a31358292a97af84dbe2b4ff5
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-03-04 06:44:18 -0800

    update error page

--- src/globalPages/error/index.jsx
+++ src/globalPages/error/index.jsx
@@ -1,5 +1,28 @@
+import { useNavigate, useRouteError } from "react-router-dom";
+
+import {
+  RiArrowGoBackLine as BackIcon,
+  RiHome2Line as HomeIcon,
+} from "react-icons/ri";
+
+import { PrimaryButton } from "components/UI";
+
 import styles from "./Error.module.css";
 
 export const ErrorPage = () => {
-  return <div className={styles.errorPage}>Error</div>;
+  const navigate = useNavigate();
+  const error = useRouteError();
+
+  return (
+    <div className={styles.errorPage}>
+      Error
+      <p>{error.message || error.status}</p>
+      <PrimaryButton onClick={() => navigate(-1)}>
+        <BackIcon /> Go back
+      </PrimaryButton>
+      <PrimaryButton onClick={() => navigate("/")}>
+        <HomeIcon /> Go home
+      </PrimaryButton>
+    </div>
+  );
 };
