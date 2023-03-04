d18ac88cd87f086949438be3f3c4e3d30863263b
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-03-03 22:22:57 -0800

    add example redux logic to homepage

--- src/features/example-feature/pages/home/index.jsx
+++ src/features/example-feature/pages/home/index.jsx
@@ -1,5 +1,33 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import { PrimaryButton } from "components/UI";
+
+import {
+  incrementAction,
+  decrementAction,
+  incrementByAmountAction,
+} from "features/example-feature/stores/counterSlice";
+
 import styles from "./Home.module.css";
 
 export const HomePage = () => {
-  return <div className={styles.homePage}>index</div>;
+  const dispatch = useDispatch();
+
+  const value = useSelector((state) => state.counter.value);
+
+  return (
+    <div className={styles.homePage}>
+      index
+      <div>{value}</div>
+      <PrimaryButton onClick={() => dispatch(incrementAction())}>
+        Increment
+      </PrimaryButton>
+      <PrimaryButton onClick={() => dispatch(decrementAction())}>
+        Decrement
+      </PrimaryButton>
+      <PrimaryButton onClick={() => dispatch(incrementByAmountAction(22))}>
+        Increment by 22
+      </PrimaryButton>
+    </div>
+  );
 };
