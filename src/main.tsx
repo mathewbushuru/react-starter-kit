5ea4ac141762814edf1dad60e3c2daf8b9cb273d
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-04-16 03:34:16 -0700

    app provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+import AppProvider from '@/providers'
 
 import "@/normalize.css"
 import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppProvider />
   </React.StrictMode>,
 )
---
+++ src/providers/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const AppProvider: FC = () => {
+  return <>App</>;
+};
+
+export default AppProvider;
