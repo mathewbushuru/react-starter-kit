aeab6cd0440211f40bcc4f6eb70447b86d5e8af5
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-04-17 19:33:54 -0700

    deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/react-starter-kit/",
   plugins: [react()],
   resolve: {
     alias: {
