fc2406f2788114e8e89033ef84fc7f03c867fe55
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-04-18 07:43:55 -0700

    deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",      // Relace with name of repository
+  // base: "/<REPO_NAME>/",      // Relace with name of repository
   plugins: [react()],
   resolve: {
     alias: {
