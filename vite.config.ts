c98ca89994d1caa468edf8e846f9ec54271ceadb
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-04-17 20:08:24 -0700

    build settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: {
-  //     "@": "/src",
-  //   },
-  // },
   resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
-   },
+    alias: {
+      "@": "/src",
+    },
+  },
   server: {
     port: 3000,
   },
