715f1961d86902a6cbc70f4d403bd39159c9b2fc
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-04-17 17:26:58 -0700

    Modal styling

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -14,7 +14,9 @@ const ModalPortal = ({
 }: ModalPrimitive.AlertDialogPortalProps) => {
   return (
     <ModalPrimitive.Portal className={cn(className)} {...props}>
-      <div className="fixed inset-0 z-50 flex items-end justify-center sm:items-center">{children}</div>
+      <div className="fixed inset-0 z-50 flex items-end justify-center sm:items-center">
+        {children}
+      </div>
     </ModalPrimitive.Portal>
   );
 };
@@ -23,7 +25,16 @@ const ModalOverlay = React.forwardRef<
   React.ElementRef<typeof ModalPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof ModalPrimitive.Overlay>
 >(({ className, children, ...props }, ref) => {
-  return <ModalPrimitive.Overlay className="fixed inset-0 z-50 bg-background/80 backdrop-blur-sm transition-opacity animate-in fade-in" {...props} ref={ref} />;
+  return (
+    <ModalPrimitive.Overlay
+      className={cn(
+        "fixed inset-0 z-50 bg-background/80 backdrop-blur-sm transition-opacity animate-in fade-in",
+        className
+      )}
+      {...props}
+      ref={ref}
+    />
+  );
 });
 
 export const ModalContent = React.forwardRef<
@@ -33,7 +44,14 @@ export const ModalContent = React.forwardRef<
   return (
     <ModalPortal>
       <ModalOverlay />
-      <ModalPrimitive.Content ref={ref} className="fixed z-50 grid w-full max-w-lg scale-100 gap-4 border bg-background p-6 opacity-100 shadow-lg animate-in fade-in-90 slide-in-from-bottom-10 sm:rounded-lg sm:zoom-in-90 sm:slide-in-from-bottom-0 md:w-full" {...props} />
+      <ModalPrimitive.Content
+        ref={ref}
+        className={cn(
+          "fixed z-50 grid w-full max-w-lg scale-100 gap-4 border bg-background p-6 opacity-100 shadow-lg animate-in fade-in-90 slide-in-from-bottom-10 sm:rounded-lg sm:zoom-in-90 sm:slide-in-from-bottom-0 md:w-full",
+          className
+        )}
+        {...props}
+      />
     </ModalPortal>
   );
 });
@@ -42,40 +60,56 @@ export const ModalHeader = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => {
-  return <div className="" {...props} />;
+  return (
+    <div
+      className={cn(
+        "flex flex-col space-y-2 text-center sm:text-left",
+        className
+      )}
+      {...props}
+    />
+  );
 };
 
 export const ModalFooter = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => {
-  return <div className="" {...props} />;
+  return (
+    <div
+      className={cn(
+        "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+        className
+      )}
+      {...props}
+    />
+  );
 };
 
 export const ModalTitle = React.forwardRef<
   React.ElementRef<typeof ModalPrimitive.Title>,
   React.ComponentPropsWithoutRef<typeof ModalPrimitive.Title>
 >(({ className, ...props }, ref) => {
-  return <ModalPrimitive.Title ref={ref} className="" {...props} />;
+  return <ModalPrimitive.Title ref={ref} className={cn("text-lg font-semibold",className)} {...props} />;
 });
 
 export const ModalDescription = React.forwardRef<
   React.ElementRef<typeof ModalPrimitive.Description>,
   React.ComponentPropsWithoutRef<typeof ModalPrimitive.Description>
 >(({ className, ...props }, ref) => {
-  return <ModalPrimitive.Description ref={ref} className="" {...props} />;
+  return <ModalPrimitive.Description ref={ref} className={cn("text-sm  text-muted-foreground",className)} {...props} />;
 });
 
 export const ModalAction = React.forwardRef<
   React.ElementRef<typeof ModalPrimitive.Action>,
   React.ComponentPropsWithoutRef<typeof ModalPrimitive.Action>
 >(({ className, ...props }, ref) => {
-  return <ModalPrimitive.Action ref={ref} className="" {...props} />;
+  return <ModalPrimitive.Action ref={ref} className={cn(className)} {...props} />;
 });
 
 export const ModalCancel = React.forwardRef<
   React.ElementRef<typeof ModalPrimitive.Cancel>,
   React.ComponentPropsWithoutRef<typeof ModalPrimitive.Cancel>
 >(({ className, ...props }, ref) => {
-  return <ModalPrimitive.Cancel ref={ref} className="" {...props} />;
+  return <ModalPrimitive.Cancel ref={ref} className={cn("mt-2 sm:mt-0")} {...props} />;
 });
