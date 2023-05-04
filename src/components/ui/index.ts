8a45e63bed02f38b9d2dc0e1ec43731674b739dc
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-05-03 21:48:04 -0700

    example components progress

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -7,31 +7,31 @@ export * from "@/components/ui/Button";
 export * from "@/components/ui/Calendar";
 export * from "@/components/ui/Card";
 export * from "@/components/ui/Checkbox";
-export * from "@/components/ui/Collapsible";
-export * from "@/components/ui/Command";
-export * from "@/components/ui/ContextMenu";
-export * from "@/components/ui/Dialog";
+export * from "@/components/ui/Collapsible"; //TODO: example
+export * from "@/components/ui/Command"; // TODO: example
+export * from "@/components/ui/ContextMenu"; // TODO: example
+export * from "@/components/ui/Dialog"; // TODO: example
 export * from "@/components/ui/Drawer";
 export * from "@/components/ui/DropdownMenu";
-export * from "@/components/ui/HoverCard";
+export * from "@/components/ui/HoverCard"; // TODO: example
 export * from "@/components/ui/Input";
 export * from "@/components/ui/Label";
-export * from "@/components/ui/MenuBar";
+export * from "@/components/ui/MenuBar"; // TODO: example
 export * from "@/components/ui/Modal";
-export * from "@/components/ui/NavigationMenu";
+export * from "@/components/ui/NavigationMenu"; // TODO: example
 export * from "@/components/ui/Popover";
-export * from "@/components/ui/Progress";
-export * from "@/components/ui/RadioGroup";
-export * from "@/components/ui/ScrollArea";
-export * from "@/components/ui/Select";
+export * from "@/components/ui/Progress"; // TODO: example
+export * from "@/components/ui/RadioGroup"; // TODO: example
+export * from "@/components/ui/ScrollArea"; // TODO: example
+export * from "@/components/ui/Select"; // TODO: example
 export * from "@/components/ui/Separator";
-export * from "@/components/ui/Skeleton";
-export * from "@/components/ui/Slider";
+export * from "@/components/ui/Skeleton"; // TODO: example
+export * from "@/components/ui/Slider"; // TODO: example
 export * from "@/components/ui/Switch";
-export * from "@/components/ui/Tabs";
-export * from "@/components/ui/Textarea";
+export * from "@/components/ui/Tabs"; // TODO: example
+export * from "@/components/ui/Textarea"; // TODO: example
 export * from "@/components/ui/Toast";
 export * from "@/components/ui/Toaster";
-export * from "@/components/ui/Toggle";
+export * from "@/components/ui/Toggle"; // TODO: example
 export * from "@/components/ui/Tooltip";
 export * from "@/components/ui/Typography";
